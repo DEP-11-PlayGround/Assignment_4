2d43f30d55793f9786ce87ceb9317cf98bfd671c
RavinduNKK <ravindunkkudagama@gmail.com>
2023-08-14 02:27:51 +0530

    algorithm assignment 1 task2 completed

--- assignment4task2.java
+++ assignment4task2.java
@@ -1,27 +1,25 @@
 public class assignment4task2 {
+
     public static void main(String[] args) {
-        int rows = 4; // Number of rows in the pattern
+        int rows = 5; // Number of rows in the pattern
         
         int i = 0;
         while (i < rows) {
             int j = 0;
             
-            
-            while (j < i) {
+            while (j < rows - i - 1) {
                 System.out.print("+");
                 j++;
             }
             
-            j = 0;
-            
-            while (j < (2 * (rows - i)) - 1) {
-                System.out.print(j == 0 || j == (2 * (rows - i)) - 2 ? "+" : " ");
+            j = 1;
+            while (j < (i * 2) + 1) {
+                System.out.print(" ");
                 j++;
             }
             
             j = 0;
-            
-            while (j < i) {
+            while (j < rows - i - 1) {
                 System.out.print("+");
                 j++;
             }
